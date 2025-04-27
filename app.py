b5f7e7dbcb1119ccc45c546046c5038051c20fd7
PH-C25 <paulinho.hrc@outlook.com>
2025-04-26 22:49:07 -0300

    Site Web Atualizado (Com Python V2)

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    mensagem = "Hello, World!"
+    mensagem = "Hello, World!"  # A mensagem para passar para o HTML
     return render_template('index.html', mensagem=mensagem)
 
 if __name__ == '__main__':
-    print("Hello, World!")
+    print("Hello, World!")  # Isso será impresso no terminal quando o servidor for iniciado
     app.run(debug=True)
